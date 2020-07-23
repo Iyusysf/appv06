236329fb2590fd21cc68df3c4a6b0b507bf79f10
Iyusysf <42061520+Iyusysf@users.noreply.github.com>
2020-07-24 06:05:38 +0700

    add files aditional files

---
+++ src/config/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAd2t_-KYygVf5iJUQNc44YXPtWu6PdLXU",
+  authDomain: "appchatv01.firebaseapp.com",
+  databaseURL: "https://appchatv01.firebaseio.com",
+  projectId: "appchatv01",
+  storageBucket: "appchatv01.appspot.com",
+  messagingSenderId: "1034171900730",
+  appId: "1:1034171900730:web:53dbf8f78b72c06dd42603"
+};
+export default firebaseConfig;
